ae122836fbe5ceb43f32eeaab4350d904eeaee0e
IevaSta <ieva@kontraforma.lt>
2022-10-17 17:46:06 +0300

    App start; buttons;

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+           <button className='grey'>1</button>
+      <button className='red'>1</button>
+      <button className='green'>1</button>
+    </div>
+  );
+}
+
+export default App;
